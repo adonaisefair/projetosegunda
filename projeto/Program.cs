964f3a8834644cdcdc0957a46de2db25415d1e27
adonaisefair <adonaisefair707@gmail.com>
2024-05-17 14:52:02 -0300

    Testando 17/05/24

--- projeto/Program.cs
+++ projeto/Program.cs
@@ -10,5 +10,6 @@ var app = builder.Build();//testando o comite
 app.MapGet("/", () => "Hello World,let's get it aaaa");
 app.MapGet("/", () => "Teste na api,criando....");
 app.MapGet("/", () => "Teste 04/05/24");
+app.MapGet("/", () => "Novo teste 17/05/24");
 app.Run();
 
